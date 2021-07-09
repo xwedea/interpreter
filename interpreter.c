bf0c525d9cbc51f3453a5a7779155df68477263d
Mahdi Sarhangi <52277462+ELDERGARLIC@users.noreply.github.com>
2021-07-09 20:00:02 +0300

    "" solved!

--- interpreter.c
+++ interpreter.c
@@ -42,6 +42,30 @@ int variableIndex = 0;
 int values[1000] = { 0 };
 int line = 1;
 
+char *substring(char *string, int position, int length)
+{
+   char *p;
+   int c;
+
+   p = malloc(length+1);
+
+   if (p == NULL)
+   {
+      printf("Unable to allocate memory.\n");
+      exit(1);
+   }
+
+   for (c = 0; c < length; c++)
+   {
+      *(p+c) = *(string+position-1);
+      string++;
+   }
+
+   *(p+c) = '\0';
+
+   return p;
+}
+
 bool variableExists(char *variable){
     if(variableIndex == 0)
         return false;
